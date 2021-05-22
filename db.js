7bde78f7149a1c3137f806ecf3f0170f879bd70a
EC2 Default User <ec2-user@ip-172-31-37-117.us-east-2.compute.internal>
2021-05-22 18:25:03 +0000

    try 2

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 const Sequelize = require('sequelize');
-const { DataTypes: {DATE,STRING, UUID, UUIDV4}} = Sequelize;
+const { DataTypes: {INTEGER, DATE,STRING, UUID, UUIDV4}} = Sequelize;
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acmemembers');
 
 const members = ['moe','lucy','larry','ethyl'];
 const facilities = ['tennis','ping-pong','raquet-ball','bowling'];
+
+//const Bookings = conn.define('booking', {
+//  
+//}
 
 const Facility = conn.define('facility', {
   id: {
@@ -35,7 +39,31 @@ const Member = conn.define('member', {
   }
 })
 
+const Booking = conn.define('booking', {
+  id: {
+    type: INTEGER,
+    primaryKey: true
+  },
+  startTime: {
+    type: DATE,
+    allowNull: false
+  },
+  endTime: { 
+    type: DATE,
+    allowNull: false
+  },
+  bookedById: {
+    type: UUID
+  },
+  facilityID: {
+    type: UUID
+  }
 
+})
+
+Member.hasMany(Booking)
+Facility.hasMany(Booking)
+Member.hasMany(Booking)
 
 const syncAndSeed = async()=>{
   await conn.sync({force:true})
@@ -43,10 +71,14 @@ const syncAndSeed = async()=>{
   const [moe, lucy, larry, ethyl] = await Promise.all(members.map(firstName=>Member.create({firstName})));
   console.log('sync and seed')
 
+  moe.sponsorId = lucy.id
+  ethyl.sponsorId = moe.id
+  larry.sponsorId = lucy.id
+  await Promise.all([moe.save(), ethyl.save(), larry.save()]);
 }
 
 
 module.exports = {
   syncAndSeed,
-  
+ models: {Member, Facility, Booking}
 }
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+const express  = require('express')
 const db = require('./db');
+const { models: {Member, Facility}} = db
+const app = express()
+
+app.get('/api/members', async(req, res, next)=> {
+  try{
+    res.send(await Member.findAll({
+      include: [ Member, Facility ]
+    }))
+  }
+  catch(ex){
+    next(ex);
+  }
+})
+
 
 const setUp = async()=> {
   try {
     await db.syncAndSeed();
-    
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`))
   }
   catch(ex){
     console.log(ex);
